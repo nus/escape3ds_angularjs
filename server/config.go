32fc02ea8610ef799b3f8ac606b56e696e27b63d
Yuta Okano <yuta.okano@gmail.com>
2013-05-27 20:23:53 +0900

    Twitterアクセストークンの取得

--- server/config.go
+++
@@ -1,9 +0,0 @@
-/**
- * 設定ファイル
- */
-package escape3ds
-var config = map[string]string {
-	"consumer_key": "j44BdfM2pzUISfPc5s3YZw",
-	"token": "283857182-A80yyby9E8raUYaLKHZ2nllSBOTDBKVjcyoKeBHn",
-	"url": "test",
-}
